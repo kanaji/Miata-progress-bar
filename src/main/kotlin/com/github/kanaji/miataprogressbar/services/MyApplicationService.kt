5cf909b4b58bf9afa10c453da89553059143e36c
GitHub Action <action@github.com>
2021-10-05 16:38:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/kanaji/miataprogressbar/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kanaji/miataprogressbar/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kanaji.miataprogressbar.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kanaji.miataprogressbar.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kanaji/miataprogressbar/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kanaji/miataprogressbar/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kanaji.miataprogressbar.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kanaji.miataprogressbar.MyBundle
 
 class MyProjectService(project: Project) {
 
